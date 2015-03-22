417869f9ec20db9b1a8b0250471cfc050b282afa
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2015-03-22 23:48:51 +0900

    Add more test case

--- test.py
+++ test.py
@@ -11,12 +11,25 @@ class TestFunctions(unittest.TestCase):
 		plugboard = pyenigma.Plugboard('')
 		rotors = [pyenigma.ROTOR1, pyenigma.ROTOR2, pyenigma.ROTOR3]
 		enigma = pyenigma.Enigma(rotors, pyenigma.REFLECTOR_B, plugboard, turnover=True)
-		enigma.setWindowCharacters('AAA')
 		enigma.setRingSettings('AAA')
+
+		enigma.setWindowCharacters('AAA')
 		self.failUnlessEqual(enigma.encode('AAAAA'), 'BDZGO')
 		enigma.setWindowCharacters('AAA')
-		enigma.setRingSettings('AAA')
 		self.failUnlessEqual(enigma.encode('BDZGO'), 'AAAAA')
+
+	# test for different ring settings
+	# from wikipedia example: http://en.wikipedia.org/wiki/Enigma_rotor_details#Ring_setting
+	def test2(self):
+		plugboard = pyenigma.Plugboard('')
+		rotors = [pyenigma.ROTOR1, pyenigma.ROTOR2, pyenigma.ROTOR3]
+		enigma = pyenigma.Enigma(rotors, pyenigma.REFLECTOR_B, plugboard, turnover=True)
+		enigma.setRingSettings('BBB')
+
+		enigma.setWindowCharacters('AAA')
+		self.failUnlessEqual(enigma.encode('AAAAA'), 'EWTYX')
+		enigma.setWindowCharacters('AAA')
+		self.failUnlessEqual(enigma.encode('EWTYX'), 'AAAAA')
 
 if __name__ == '__main__':
 	unittest.main()
